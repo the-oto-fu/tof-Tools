3f5aaecbfbfe4d79a446b4f2667a50a22d8b5d1a
the-oto-fu <yosukenaritaster@gmail.com>
2023-04-02 03:59:04 +0900

    ヘッダメニューのトグルを追加中 うまく動かないので一旦ここまで

--- src/components/header.js
+++ src/components/header.js
@@ -1,27 +1,67 @@
-import React from 'react'
-import { Menu, Icon, Dropdown } from 'semantic-ui-react'
+import { useState, useRef } from 'react'
+import { Menu, Icon } from 'semantic-ui-react'
+
+//https://qiita.com/G4RDSjp/items/58364a6655d4968a90d9
 
 const handleClickSiteName = () => {
   console.log('clicked.')
 }
 
-function SiteHeader() {
+function HeaderMenu() {
   return (
-    <Menu borderless fixed='top' color='blue' inverted>
-      <Menu.Item
-        onClick={handleClickSiteName}
-      >
-        <h4>tof-Tools</h4>
+    <Menu vertical>
+      <Menu.Item href='//example.com' target='_blank'>
+        Visit another website
       </Menu.Item>
-      <Menu.Menu position='right'>
-        <Menu.Item>
-          <div className="header">
-            MENU
-            <Icon name='bars' />
-          </div>
-        </Menu.Item>
-      </Menu.Menu>
+      <Menu.Item>Link via prop</Menu.Item>
+      <Menu.Item>Javascript Link</Menu.Item>
     </Menu>
+  )
+}
+
+function SiteHeader() {
+
+  const [isShown, setIsShown] = useState(false)
+
+  const handelToggleMenu = () => {
+    setIsShown(!isShown)
+    document.addEventListener('click', documentClickHander)  
+  }
+
+  const documentClickHander = e => {
+//    if(headerMenuRef.current.containts(e.target)){
+//      return
+//    }
+
+    setIsShown(false)
+    document.removeEventListener('click', documentClickHander)
+  }
+
+  const headerMenuRef = useRef()
+
+  return (
+    <>
+      <Menu borderless fixed='top' color='blue' inverted>
+        <Menu.Item
+          onClick={handleClickSiteName}
+        >
+          <h4>tof-Tools</h4>
+        </Menu.Item>
+        <Menu.Menu position='right'>
+          <Menu.Item onClick={handelToggleMenu}>
+            <div className="header-menubar">
+              MENU
+              <Icon name='bars' />
+            </div>
+          </Menu.Item>
+        </Menu.Menu>
+      </Menu>
+      <div className={`headerMenu ${isShown ? 'shown' : ''}`}
+        ref={headerMenuRef}
+      >
+        <HeaderMenu />
+      </div>
+    </>
   )
 }
 
