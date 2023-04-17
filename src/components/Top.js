a43b18f231f1f105a808fd5039145a8c0729414f
the-oto-fu <yosukenaritaster@gmail.com>
2023-04-17 22:01:05 +0900

    トップをちょっとおしゃれにしたい

--- src/components/Top.js
+++ src/components/Top.js
@@ -1,19 +1,35 @@
+import { useEffect } from 'react'
 import { Container, Header } from 'semantic-ui-react'
+import { gsap } from 'gsap'
 import { motion } from 'framer-motion'
 
 function Top() {
+    const shapeRef = useRef();
+    useEffect(() => {
+      gsap.to(div.current, {
+        backgroundImage: "linear-gradient(to left, #30CFD0, #330867)",
+        duration: 5
+      });
+    }, [div]);
+
     return (
         <motion.div
             animate={{ opacity: 1 }}
             initial={{ opacity: 0 }}
             transition={{ duration: 0.2 }}
         >
-            <Container className='main'>
-                <Header as='h2'>Welcome！</Header>
-                <p>
-                    サイトの説明を記載
-                </p>
-            </Container>
+            <div class="shapes">
+                <div class="shape shape1"></div>
+                <div class="shape shape2"></div>
+                <div class="shape shape3"></div>
+            </div>
+
+            <div class="content">
+                <h1>tofTool</h1>
+            </div>
+            <p>
+                サイトの説明を記載
+            </p>
         </motion.div>
     )
 }
